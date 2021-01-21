51d6c216a583fcd6dc51378406ca819081a45e56
Pera <alex96pro@gmail.com>
2021-01-21 14:40:41 +0100

    added new query for tags

--- src/api/user.js
+++ src/api/user.js
@@ -6,23 +6,24 @@ import { DAILY_SPECIALS_PER_PAGE } from '../consts.js';
 export async function feed(req,res) {
     try{
         let decodedEmail = decodeToken(req.headers.authorization);
-        let deliveryQuery = '';
-        let tagsQuery = '';
-        if(req.query.tags !== 'null'){
-            let tagsArray = req.query.tags.split(',');
-            tagsQuery = 'AND (';
-            for(let i = 0; i < tagsArray.length; i++){
-                tagsQuery += `tags LIKE '%${tagsArray[i]}%' OR `;
-            }
-            tagsQuery = tagsQuery.substring(0, tagsQuery.length - 4);
-            tagsQuery += ')';
-        }
-        if(req.query.delivery === 'true'){
-            deliveryQuery = 'AND delivery = true';
-        }
         if(decodedEmail === null){
             return res.status(401).json("UNAUTHORIZED");
         }else{
+            let deliveryQuery = '';
+            let tagsQuery = '';
+            if(req.query.tags !== 'null'){
+                let tagsArray = req.query.tags.split(',');
+                tagsQuery = 'AND (';
+                for(let i = 0; i < tagsArray.length; i++){
+                    tagsQuery += `tags LIKE '%${tagsArray[i]}%' OR `;
+                }
+                tagsQuery = tagsQuery.substring(0, tagsQuery.length - 4);
+                tagsQuery += ')';
+            }
+            if(req.query.delivery === 'true'){
+                deliveryQuery = 'AND delivery = true';
+            }
+            
             let result = await pool.query(`SELECT DISTINCT ON(specials."specialId") specials."specialId" AS "mealId", sqrt((("lat" - $1) * 111)^2 + (("lon" - $2) * 111)^2) AS "distance", "location", restaurants.name as "restaurantName", specials.name as "mealName", "photo", "price", "tags", "delivery", "delivery-minimum", "phone", "restaurantId" `+
             `FROM specials JOIN restaurants USING("restaurantId") `+
             `WHERE sqrt((("lat" - $1) * 111)^2 + (("lon" - $2) * 111)^2) < $3 AND ("delivery" = false OR (sqrt((("lat" - $1) * 111)^2 + (("lon" - $2) * 111)^2) < "delivery-range" AND "delivery"=true)) ${tagsQuery} ${deliveryQuery} LIMIT $4 OFFSET $5`,
