b0a1f6d317c98a0166da1c2e07cf0a230b9858ba
Pera <alex96pro@gmail.com>
2021-02-14 20:24:54 +0100

    initial commit to heroku

--- src/api/user.js
+++ src/api/user.js
@@ -6,6 +6,7 @@ export async function feed(req,res) {
     try{
         let deliveryQuery = '';
         let tagsQuery = '';
+        let searchQuery = '';
         if(req.query.tags !== 'null'){
             let tagsArray = req.query.tags.split(',');
             tagsQuery = 'AND (';
@@ -18,10 +19,13 @@ export async function feed(req,res) {
         if(req.query.delivery === 'true'){
             deliveryQuery = 'AND delivery = true';
         }
+        if(req.query.search){
+            searchQuery = `AND ("tags" ILIKE '%${req.query.search}%' OR specials.name ILIKE '%${req.query.search}%')`;
+        }
         
         let result = await pool.query(`SELECT DISTINCT ON(specials."specialId") specials."specialId" AS "mealId", sqrt((("lat" - $1) * 111)^2 + (("lon" - $2) * 111)^2) AS "distance", "location", restaurants.name as "restaurantName", specials.name as "mealName", "photo", "price", "tags", "description", "delivery", "delivery-minimum", "phone", "restaurantId", "working-hours-from"[${req.query.day}], "working-hours-to"[${req.query.day}] `+
         `FROM specials JOIN restaurants USING("restaurantId") `+
-        `WHERE sqrt((("lat" - $1) * 111)^2 + (("lon" - $2) * 111)^2) < $3 AND ("delivery" = false OR (sqrt((("lat" - $1) * 111)^2 + (("lon" - $2) * 111)^2) < "delivery-range" AND "delivery"=true)) ${tagsQuery} ${deliveryQuery} AND "date" = $4 LIMIT $5 OFFSET $6`,
+        `WHERE sqrt((("lat" - $1) * 111)^2 + (("lon" - $2) * 111)^2) < $3 AND ("delivery" = false OR (sqrt((("lat" - $1) * 111)^2 + (("lon" - $2) * 111)^2) < "delivery-range" AND "delivery"=true)) ${tagsQuery} ${deliveryQuery} ${searchQuery} AND "date" = $4 LIMIT $5 OFFSET $6`,
         [req.query.lat, req.query.lon, req.query.range, req.query.date, DAILY_SPECIALS_PER_PAGE, (req.query.scrollCount - 1) * DAILY_SPECIALS_PER_PAGE]);
         if(result.rows.length && result.rows.length > 0){
             convertTagsToArray(result.rows);
@@ -48,7 +52,7 @@ export async function menu(req,res) {
         `FROM meals JOIN restaurants USING("restaurantId") WHERE "restaurantId" = $1`,[req.params.id]);
         if(result.rows.length && result.rows.length > 0){
             convertTagsToArray(result.rows);
-            let result2 = await pool.query('SELECT "restaurantId", "name" as "restaurantName", "location", "delivery", "delivery-minimum" as "deliveryMinimum", "phone", "categories" FROM restaurants WHERE "restaurantId" = $1',[req.params.id]);
+            let result2 = await pool.query('SELECT "restaurantId", "name" as "restaurantName", "location", "delivery", "delivery-minimum" as "deliveryMinimum", "phone", "categories", "logo" FROM restaurants WHERE "restaurantId" = $1',[req.params.id]);
             if(result2.rows.length && result2.rows.length > 0){
                 result2.rows[0].categories = convertStringToArray(result2.rows[0].categories);
                 res.json({meals: result.rows, restaurant: result2.rows[0]});
