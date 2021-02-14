e12c376f4863cf53981f187345ac1f3ac142e13c
Pera <alex96pro@gmail.com>
2021-02-14 23:30:00 +0100

    changed sendgrid sender

--- src/config/sendGrid.js
+++ src/config/sendGrid.js
@@ -7,7 +7,7 @@ export async function sendVerifyEmail(email, hashedUserId) {
     try{
         sendGridMail.setApiKey(process.env.SENDGRID_API_KEY);
         const msg = {
-            to: 'foodytransparentservice@gmail.com',
+            to: 'directdailyspecials@gmail.com',
             from: process.env.SENDER_EMAIL,
             templateId: 'd-e458506a14c549b0a373e62d7e1f1063',
             dynamic_template_data: {
@@ -28,7 +28,7 @@ export async function sendForgottenPassword(email, hashedUserId) {
         sendGridMail.setApiKey(process.env.SENDGRID_API_KEY);
         const msg = {
             to: email,
-            from: 'foodytransparentservice@gmail.com',
+            from: 'directdailyspecials@gmail.com',
             templateId: 'd-e5979cea9cb0401091f34b2d2c0cea1e',
             dynamic_template_data: {
                 FRONTEND_API: FRONTEND_API,
@@ -48,7 +48,7 @@ export async function sendVerifyEmailRestaurant(email, hashedRestaurantId) {
         sendGridMail.setApiKey(process.env.SENDGRID_API_KEY);
         const msg = {
             to: email,
-            from: 'foodytransparentservice@gmail.com',
+            from: 'directdailyspecials@gmail.com',
             templateId: 'd-a2248d3a20a142aa96d68d284a834b25',
             dynamic_template_data: {
                 restaurantId: hashedRestaurantId,
@@ -68,7 +68,7 @@ export async function sendForgottenPasswordRestaurant(email, hashedRestaurantId)
         sendGridMail.setApiKey(process.env.SENDGRID_API_KEY);
         const msg = {
             to: email,
-            from: 'foodytransparentservice@gmail.com',
+            from: 'directdailyspecials@gmail.com',
             templateId: 'd-028dd4aecf6949218ac151fdf9d07372',
             dynamic_template_data: {
                 FRONTEND_API: FRONTEND_API,
